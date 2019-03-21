e1c041ce78682896955b76fc4a4d5dccff0a4558
Kamil Grodzki <48187680+Cotusiedzieje@users.noreply.github.com>
2019-03-21 20:35:05 +0100

    Add files via upload

---
+++ Paradygmaty_Programowania/Lab01/Zad7/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let x = 25
+let y = 46
+printf("%u %u\n") x y
+printf("     %u %u\n") x y
+printf("000000%u 000000%u\n") x y
+if x>0 then
+    if y>0 then
+        printf("+%u +%u\n") x y
+    elif y<0 then
+        printf("+%u %u\n") x y
+elif x<0 then
+    if y>0 then
+        printf("%u +%u\n") x y
+    elif y<0 then
+        printf("%u %u\n") x y
+printf("%o %o\n") x y
+printf("%X %X\n") x y
+Console.ReadKey() |> ignore
