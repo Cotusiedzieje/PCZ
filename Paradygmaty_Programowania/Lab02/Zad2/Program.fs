bc0e0acde4de3b559bda65b409f2937853243415
Kamil Grodzki <48187680+Cotusiedzieje@users.noreply.github.com>
2019-03-21 21:57:37 +0100

    Add files via upload

---
+++ Paradygmaty_Programowania/Lab02/Zad2/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+[<EntryPoint>]
+let main argv = 
+    let sprawdzenie (x,y) =
+        if x%4 = y%4 then true
+        else false
+    Console.WriteLine(sprawdzenie (5,5)) 
+    Console.ReadKey() |> ignore
+    0
