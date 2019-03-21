66f00b9bf9f26faf651e0ec29090c25930431e7c
Kamil Grodzki <48187680+Cotusiedzieje@users.noreply.github.com>
2019-03-21 19:01:41 +0100

    Add files via upload

---
+++ Paradygmaty_Programowania/Lab01/Zad1/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let a= "Imie Nazwisko"
+    Console.WriteLine("Moje imie i nazwisko: {0}",a)
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
