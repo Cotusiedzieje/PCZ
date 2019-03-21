d48f5a8cba143180a672596d274b6c3d3be8ad67
Kamil Grodzki <48187680+Cotusiedzieje@users.noreply.github.com>
2019-03-21 20:13:49 +0100

    Add files via upload

---
+++ Paradygmaty_Programowania/Lab01/Zad6/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+let main argv =
+    let rec rekurencja a n q =
+      if n<2 then
+           a
+       else 
+           q * rekurencja a (n-1) q
+
+    Console.WriteLine(rekurencja 1 2 5)
+Console.ReadKey() |> ignore
