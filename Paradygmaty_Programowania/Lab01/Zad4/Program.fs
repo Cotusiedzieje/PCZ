9b45322bfd31c27e2e8b1dd10a36e50ad256a30e
Kamil Grodzki <48187680+Cotusiedzieje@users.noreply.github.com>
2019-03-21 19:24:25 +0100

    Add files via upload

---
+++ Paradygmaty_Programowania/Lab01/Zad4/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let kwadrat a: float =
+    a * a
+let kw = kwadrat 4.2
+printf "Kwadrat: %g\n" kw
+
+let prostokat b c: float =
+    b * c
+let p = prostokat 2.4 4.3
+printf "Prostokąt: %g\n" p
+
+let kolo r: float =
+    let pi = 3.14
+    pi * r * r
+let ko = kolo 3.2
+printf "Koło: %g\n" ko
+Console.ReadKey() |> ignore
