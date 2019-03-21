46e373a8932870bed775eea9789b50d10b8c2f03
Kamil Grodzki <48187680+Cotusiedzieje@users.noreply.github.com>
2019-03-21 22:01:26 +0100

    Add files via upload

---
+++ Paradygmaty_Programowania/Lab02/Zad5/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+[<EntryPoint>]
+let main argv = 
+    let funkcja (x,y) =
+        match x%y with
+        | 0-> None
+        | _-> Some (x%y)
+    Console.WriteLine(funkcja (20,12)) 
+    Console.ReadKey() |> ignore
+    0
