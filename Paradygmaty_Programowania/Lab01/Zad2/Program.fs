25fabfdbc34b98a63b3b389d9fde37817ed71b64
Kamil Grodzki <48187680+Cotusiedzieje@users.noreply.github.com>
2019-03-21 19:09:48 +0100

    Add files via upload

---
+++ Paradygmaty_Programowania/Lab01/Zad2/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+
+printf "Podaj swoje imię: " 
+let a = Console.ReadLine() 
+printf "Podaj swoje nazwisko: " 
+let b = Console.ReadLine() 
+printf "Twoje imie i nazwisko: %s %s" a b
+Console.ReadKey() |> ignore
